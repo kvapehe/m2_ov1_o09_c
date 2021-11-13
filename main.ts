28dafebd627c0eae6bb27dbacdf7ada051e02d2f
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-13 02:26:46 +0100

    Levert

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function () {
+    tall += 1
+})
+let tall = 0
+basic.showNumber(tall)
+basic.forever(function () {
+    tall = tall % 10
+    basic.showNumber(tall)
+})
